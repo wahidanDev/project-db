96cc9420a25318f6bd58aebcb35fd422b4cb4f3a
wahidanDev <wahidann890@gmail.com>
2025-08-25 14:00:16 +0700

    functionn

--- tugas/function.sql
+++ tugas/function.sql
@@ -3,16 +3,25 @@ CREATE DATABASE IF NOT EXISTS hitung_luas;
 USE hitung_luas;
 
 DELIMITER //
+CREATE FUNCTION sf_hitungluas (
+    panjang tinyint,
+    lebar tinyint
+) RETURNS INT DETERMINISTIC
+BEGIN
+    RETURN panjang * lebar;
+END //
+DELIMITER ;
+
+DELIMITER //
 CREATE PROCEDURE sf_hitungluas (
-    panjang INT,
-    lebar INT
+    panjang tinyint,
+    lebar tinyint
 )
 BEGIN
     set @p_panjang = panjang;
     set @p_lebar = lebar;
     SELECT sf_hitungluas(@_panjang, @lebar);
 END //
-
 DELIMITER ;
 
 -- Panggil fungsi dengan nama yang benar
